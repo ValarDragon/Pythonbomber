f8967fbbefa416f2108c19d769e5973615adfe56
ValarDragon <dojha12@gmail.com>
2016-10-23 23:04:21 -0500

    Added a logging system!

--- emailbomberv2.py
+++ emailbomberv2.py
@@ -3,25 +3,47 @@ import getpass
 import sys
 import traceback
 import configparser
+import logging
+from logging.config import fileConfig
+
 #http://stackoverflow.com/questions/19379120/how-to-read-a-config-file-using-python
-def main():
+def setupLogging():
+    fileConfig('logging.conf')
+    global logger
+    logger = logging.getLogger("root.main")
+    logger.debug("Program started")
+
+def startup():
     global username
     global password
 
     global config
     config = configparser.ConfigParser()
     config.read('config.ini')
+
     if('username' in config['email']):
         username = config['email']['username']
         print("hello " + username)
     else:
         print("Enter in your gmail username\n\r")
         username = input("Gmail username: ")
+
     if('password' in config['email']):
         password = config['email']['password']
     else:
         print("Enter in your gmail password\n\r")
         password = getpass.getpass(prompt = "Gmail password: ")
+
+    global isLogging
+    if('1' in config['email']['logging']):
+        isLogging = True
+        setupLogging()
+    else:
+        isLogging = False
+    main()
+
+def main():
+
 
     print ("""What do you want to bomb?
         1. Email
@@ -88,5 +110,8 @@ def spam(num, email, msg):
             server.sendmail(username, email, msg)
         except:
             print(str(traceback.format_exc()))
+    if(isLogging):
+        logger.debug(str(num) + ' email(s) sent to ' + email + ' with content ' + msg)
     server.quit()
-main()
+    main()
+startup()
